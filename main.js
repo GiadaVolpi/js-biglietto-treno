785524018f47974835b2bffe172b0fa1468560ef
Gi@da <giadavolpi8@gmail.com>
2019-11-15 18:13:45 +0100

    arrotondamento decimali

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ console.log (anni);
 if (isNaN (anni)) {
     // la variabile anni non contiene un numero
     alert ("Hai sbagliato a scrivere la tua età");
+    document.getElementById ("eta").innerHTML = " ...ERRORE!... ";
 } else if (anni < 0) {
     // non sei ancora nato
     alert ("Le cicogne sono gratis!");
@@ -17,6 +18,7 @@ if (isNaN (anni)) {
 } else if (anni > 120) {
     // hai superato i 120 anni
     alert ("Dovresti partecipare al Guinness Wordl Record!");
+    document.getElementById ("eta").innerHTML = " ...ERRORE!... ";
 }
 
 document.getElementById ("eta").innerHTML = anni;
@@ -29,9 +31,10 @@ console.log (km);
 if (isNaN (km)) {
     // la variabile km non contiene un numero
     alert ("Hai sbagliato a scrivere il numero dei kilometri");
-    document.getElementById ("tot-km").innerHTML = "ERRORE!";
+    document.getElementById ("tot-km").innerHTML = " ...ERRORE!...";
 } else if (km <= 0) {
     alert ("Non hai bisogno di un biglietto");
+    document.getElementById ("tot-km").innerHTML = " ...ERRORE!...";
 }
 
 document.getElementById ("tot-km").innerHTML = km + " " + "km";
@@ -39,25 +42,35 @@ document.getElementById ("tot-km").innerHTML = km + " " + "km";
 
 // calcolo costo biglietto
     // biglietto senza sconto
-var bigliettoIntero = 0.21 * km;
+var bigliettoIntero = (0.21 * km).toFixed(2);
     // biglietto con sconto del 20%
-var bigliettoMinorenni = 0.21 * km / 100 * 80;
+var bigliettoMinorenni = (0.21 * km / 100 * 80).toFixed(2);
     // biglietto con sconto del 40%
-var bigliettoAnziani = 0.21 * km / 100 * 60;
+var bigliettoAnziani = (0.21 * km / 100 * 60).toFixed(2);
 console.log (bigliettoIntero);
 console.log (bigliettoMinorenni);
 console.log (bigliettoAnziani);
 
-if (anni < 0) {
+if (anni <= 0) {
+    document.getElementById ("eta").innerHTML = " ...ERRORE!... ";
+} else if (anni <= 0 && km > 0) {
     document.getElementById ("costo-biglietto").innerHTML = "ERRORE!";
 } else if (anni < 18) {
     document.getElementById ("costo-biglietto").innerHTML = "€" + " " + bigliettoMinorenni;
+} else if (anni < 18 && km <= 0) {
+    document.getElementById ("costo-biglietto").innerHTML = "NON HAI BISOGNO DI UN BIGLIETTO!";
 } else if (anni > 18 && anni < 65) {
     document.getElementById ("costo-biglietto").innerHTML = "€" + " "  + bigliettoIntero;
+} else if (anni > 18 && anni < 65 && km <= 0) {
+    document.getElementById ("costo-biglietto").innerHTML = "NON HAI BISOGNO DI UN BIGLIETTO!";
 } else if (anni >= 65 && anni < 120) {
     document.getElementById ("costo-biglietto").innerHTML = "€" + " "  + bigliettoAnziani;
+} else if (anni >= 65 && anni < 120 && km <= 0) {
+    document.getElementById ("costo-biglietto").innerHTML = "NON HAI BISOGNO DI UN BIGLIETTO!";
 } else  if (anni > 120) {
-    document.getElementById ("costo-biglietto").innerHTML = "ERRORE!";
+    document.getElementById ("eta").innerHTML = " ...ERRORE!... ";
+} else  if (anni > 120 && km <= 0) {
+    document.getElementById ("costo-biglietto").innerHTML = "NON HAI BISOGNO DI UN BIGLIETTO!";
 }
 
 document.getElementById("calcolatore-container").setAttribute("class", "visible");
